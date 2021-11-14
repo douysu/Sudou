8d29c1e0cd54790d7544e23cad29aaca396a90a4
LAPTOP-DS21LDHG\dou <yindou97@163.com>
2021-11-14 11:06:14 +0800

    feat(build): 增加脚本，修改测试

--- Sudou/src/Sudou/EntryPoint.h
+++ Sudou/src/Sudou/EntryPoint.h
@@ -1,11 +1,12 @@
 #pragma once
-
+#include <iostream>
 #ifdef SUDOU_PLATFORM_WINDOWS
 
 extern Sudou::Application* Sudou::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	std::cout << "Hello~" << std::endl;
 	auto app = Sudou::CreateApplication();
 	app->Run();
 	delete app;
