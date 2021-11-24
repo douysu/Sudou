6a2ac1d69e0f921666071324ce571c4cdb22f417
douysu <yindou97@163.com>
2021-11-24 12:30:35 +0800

    feat(spdlog): 增加库spdlog

--- Engine/Source/Runtime/EntryPoint.h
+++ Engine/Source/Runtime/EntryPoint.h
@@ -1,11 +1,17 @@
 #pragma once
 #include <iostream>
+#include "Log.h"
 
 extern Sudou::Application* Sudou::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello~" << std::endl;
+	Sudou::Log::Init();
+	SD_CORE_WARN("Initialized Log");
+
+	int a = 5;
+	SD_INFO("Hello~ Var = {0}", a);
+
 	auto app = Sudou::CreateApplication();
 	app->Run();
 	delete app;
---
+++ Engine/Source/Runtime/Log.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <memory>
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Sudou {
+	class SUDOU_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+};
+
+// Core log macros
+#define SD_CORE_TRACE(...)    ::Sudou::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define SD_CORE_INFO(...)     ::Sudou::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define SD_CORE_WARN(...)     ::Sudou::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define SD_CORE_ERROR(...)    ::Sudou::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define SD_CORE_FATAL(...)    ::Sudou::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Client log macros
+#define SD_TRACE(...)	      ::Sudou::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define SD_INFO(...)	      ::Sudou::Log::GetClientLogger()->info(__VA_ARGS__)
+#define SD_WARN(...)	      ::Sudou::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define SD_ERROR(...)	      ::Sudou::Log::GetClientLogger()->error(__VA_ARGS__)
+#define SD_FATAL(...)	      ::Sudou::Log::GetClientLogger()->fatal(__VA_ARGS__)
