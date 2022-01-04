e4184b0091a0eb25e926fda68497206ebae870ea
douysu <yindou97@163.com>
2022-01-04 12:00:33 +0800

    feat: 修改Cmake

---
+++ Cmake/ThirdParty.cmake
@@ -0,0 +1,16 @@
+find_library(glfw_library glfw3 PATHS "${THIRD_PARTY}/GLFW")
+
+if (WIN32)
+    add_subdirectory("${THIRD_PARTY}/glad/include")
+    add_subdirectory("${THIRD_PARTY}/spdlog")
+    add_subdirectory("${THIRD_PARTY}/imgui")
+    set(BUILD_STATIC_LIBS 1)
+    add_subdirectory("${THIRD_PARTY}/glm")
+
+    set(LIBS opengl32 GLAD ImGui glm_static spdlog ${glfw_library})
+    include(${CMAKE_SOURCE_DIR}/Cmake/ConfigIDEDirectory.cmake)
+elseif (UNIX AND NOT APPLE)
+    message(STATUS "Is Unix")
+elseif (APPLE)
+    message(STATUS "Is Apple")
+endif (WIN32)
