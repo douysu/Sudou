2b5ef2cf76af65d35ddab635bba99fa3f95243ba
douysu <yindou97@163.com>
2022-01-07 10:23:26 +0800

    feat: 封装buffer layout

--- Engine/Source/Runtime/Core/Application.cpp
+++ Engine/Source/Runtime/Core/Application.cpp
@@ -12,6 +12,27 @@ namespace Sudou
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case Sudou::ShaderDataType::Float:    return GL_FLOAT;
+			case Sudou::ShaderDataType::Float2:   return GL_FLOAT;
+			case Sudou::ShaderDataType::Float3:   return GL_FLOAT;
+			case Sudou::ShaderDataType::Float4:   return GL_FLOAT;
+			case Sudou::ShaderDataType::Mat3:     return GL_FLOAT;
+			case Sudou::ShaderDataType::Mat4:     return GL_FLOAT;
+			case Sudou::ShaderDataType::Int:      return GL_INT;
+			case Sudou::ShaderDataType::Int2:     return GL_INT;
+			case Sudou::ShaderDataType::Int3:     return GL_INT;
+			case Sudou::ShaderDataType::Int4:     return GL_INT;
+			case Sudou::ShaderDataType::Bool:     return GL_BOOL;
+		}
+		
+		SD_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		SD_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -27,16 +48,37 @@ namespace Sudou
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
+		
+		{
+			BufferLayout layout = 
+			{
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+			element.GetComponentCount(),
+			ShaderDataTypeToOpenGLBaseType(element.Type),
+			element.Normalized ? GL_TRUE : GL_FALSE,
+			layout.GetStride(),
+			(const void*)element.Offset);
+			index++;
+		}
 
 		uint32_t indices[3] = { 0, 1, 2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -45,12 +87,15 @@ namespace Sudou
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);	
 			}
 		)";
@@ -61,10 +106,11 @@ namespace Sudou
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
